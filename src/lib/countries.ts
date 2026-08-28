75c95a50be83eeaa2e40dd3271e1edfc8fc4c817
Mostafa Abdelouahab <abdelouahab.mostafa.etu@centre-univ-mila.dz>
2026-08-28 08:51:57 +0100

    feat: start Afaq with Taiwan (NTU) only

--- src/lib/countries.ts
+++ src/lib/countries.ts
@@ -10,40 +10,13 @@ export type Country = {
 
 export const COUNTRIES: Country[] = [
   {
-    code: "in",
-    iso: "IN",
-    slug: "india",
-    nameAr: "الهند",
-    nameNative: "भारत",
-    nameEn: "India",
-    flag: "https://flagcdn.com/w80/in.png",
-  },
-  {
-    code: "pk",
-    iso: "PK",
-    slug: "pakistan",
-    nameAr: "باكستان",
-    nameNative: "پاکستان",
-    nameEn: "Pakistan",
-    flag: "https://flagcdn.com/w80/pk.png",
-  },
-  {
-    code: "jp",
-    iso: "JP",
-    slug: "japan",
-    nameAr: "اليابان",
-    nameNative: "日本",
-    nameEn: "Japan",
-    flag: "https://flagcdn.com/w80/jp.png",
-  },
-  {
-    code: "sa",
-    iso: "SA",
-    slug: "saudi-arabia",
-    nameAr: "السعودية",
-    nameNative: "المملكة العربية السعودية",
-    nameEn: "Saudi Arabia",
-    flag: "https://flagcdn.com/w80/sa.png",
+    code: "tw",
+    iso: "TW",
+    slug: "taiwan",
+    nameAr: "تايوان",
+    nameNative: "臺灣",
+    nameEn: "Taiwan",
+    flag: "https://flagcdn.com/w80/tw.png",
   },
 ];
 
